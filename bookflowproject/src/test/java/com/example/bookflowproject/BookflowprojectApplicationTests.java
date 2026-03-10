bc099e78eef755806724e83f6b4084ac74d22669
arafat <mridularafat735@gmail.com>
2026-03-11 02:01:29 +0600

    setup: render

---
+++ bookflowproject/src/test/java/com/example/bookflowproject/BookflowprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookflowproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookflowprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
