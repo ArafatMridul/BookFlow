619207dd17a9b21c26c4c2c9d8b755bb0c8f7548
Dip Shekhor Datta <123153255+dipshekhor@users.noreply.github.com>
2026-03-14 23:14:29 +0600

    userpage:browse catalog option is working with the wishlist (#16)

--- bookflowproject/src/main/java/com/example/bookflowproject/entity/Book.java
+++ bookflowproject/src/main/java/com/example/bookflowproject/entity/Book.java
@@ -1,12 +1,18 @@
 package com.example.bookflowproject.entity;
 
-import jakarta.persistence.*;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.util.Set;
 
 @Entity
 @Table(name = "books")
@@ -32,6 +38,9 @@ public class Book {
     private Integer publicationYear;
 
     private String publisher;
+
+    @Column(name = "cover_image_url")
+    private String coverImageUrl;
 
     @Column(name = "total_copies")
     private Integer totalCopies = 1;
